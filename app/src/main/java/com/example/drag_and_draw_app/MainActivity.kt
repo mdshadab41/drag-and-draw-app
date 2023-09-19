974b8ebb4230d36029cd2ff141fd7809059f3fea
Shadab <shadabjhu00447@gmail.com>
2023-09-19 19:20:24 +0530

    DragAndDraw

---
+++ app/src/main/java/com/example/drag_and_draw_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.drag_and_draw_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
